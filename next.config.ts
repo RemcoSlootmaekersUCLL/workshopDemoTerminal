75ecd0855d607a2a1c9852955a8890ee8124f508
RemcoSlootmaekersUCLL <r1035581@ucll.be>
2025-12-10 17:38:47 +0100

    added export config settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/workshopDemoTerminal',
+  assetPrefix: '/workshopDemoTerminal',
 };
 
 export default nextConfig;
